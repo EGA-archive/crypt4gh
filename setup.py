b043486f1ebbccf029d762e5ba0d124b02126017
Frédéric Haziza <frederic.haziza@crg.eu>
2019-11-11 17:16:41 +0100

    Hard-coding values in the setup.py and adding install_requires

--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
-import os
 import sys
-from setuptools import setup
-
 assert sys.version_info >= (3, 6), "crypt4gh requires python 3.6 or higher"
 
-from crypt4gh import __version__, __author__, __title__, __doc__ as crypt4gh_doc, __license__, PROG
-
-here = os.path.dirname(__file__)
-with open(os.path.join(here, 'requirements.txt')) as f:
-    packages = f.readlines()
+from setuptools import setup
 
 setup(name='crypt4gh',
-      version=__version__,
+      version=1,
       url='https://www.github.com/EGA-archive/crypt4gh',
-      license=__license__,
-      author=__author__,
-      #author_email='frederic.haziza@crg.eu',
-      description=__title__,
-      long_description=crypt4gh_doc,
+      license='Apache License 2.0',
+      author='Frédéric Haziza <frederic.haziza@crg.eu>',
+      description='GA4GH cryptographic utilities',
+      long_description="""The crypt4gh package is an implementation to handle the GA4GH cryptographic file format.""",
       packages=['crypt4gh'],
       include_package_data=False,
       package_data={ 'crypt4gh': ['completions'] },
       zip_safe=False,
       entry_points={
           'console_scripts': [
-              f'{PROG}=crypt4gh.__main__:main',
-              f'{PROG}-keygen=crypt4gh.keys:main',
-              f'{PROG}-debug=crypt4gh.debug:main',
+              'crypt4gh=crypt4gh.__main__:main',
+              'crypt4gh-keygen=crypt4gh.keys:main',
+              'crypt4gh-debug=crypt4gh.debug:main',
           ]
       },
       platforms = 'any',
       python_requires='>=3.6',
-      install_requires=packages,
+      # See https://packaging.python.org/discussions/install-requires-vs-requirements/
+      install_requires=[ # Should add >=x.y.z for all
+          'pyYaml',
+          'docopt',
+          'cryptography',
+          'pynacl',
+          'bcrypt',
+      ],
 )

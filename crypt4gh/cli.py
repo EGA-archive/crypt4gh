308612608a0eeb3d96b6fb37f2f39d87d154ef23
Stefan Negru <stefan@blankdots.com>
2023-01-30 10:34:49 +0200

    show message that key does not exist

--- crypt4gh/cli.py
+++ crypt4gh/cli.py
@@ -134,8 +134,8 @@ def encrypt(args):
         for pk in args['--recipient_pk']:
             recipient_pubkey = os.path.expanduser(pk)
             if not os.path.exists(recipient_pubkey):
-                msg = f"Recipient pubkey: {recipient_pubkey}, does not exist"
-                raise ValueError(msg)
+                print(f"Recipient pubkey: {recipient_pubkey}, does not exist", file=sys.stderr)
+                continue
             LOG.debug("Recipient pubkey: %s", recipient_pubkey)
             yield (0, seckey, get_public_key(recipient_pubkey))
 
@@ -196,8 +196,8 @@ def reencrypt(args):
         for pk in args['--recipient_pk']:
             recipient_pubkey = os.path.expanduser(pk)
             if not os.path.exists(recipient_pubkey):
-                msg = f"Recipient pubkey: {recipient_pubkey}, does not exist"
-                raise ValueError(msg)
+                print(f"Recipient pubkey: {recipient_pubkey}, does not exist", file=sys.stderr)
+                continue
             LOG.debug("Recipient pubkey: %s", recipient_pubkey)
             yield (0, seckey, get_public_key(recipient_pubkey))
 
